9333f99f5afd74c6df222ffa2d25a155cdf9b170
Hassan <pkhajeh@gmail.com>
2020-11-07 00:33:27 +0100

    created pokemon repository

---
+++ src/main/java/com/example/pokedex/repository/PokemonRepository.java
@@ -0,0 +1,7 @@
+package com.example.pokedex.repository;
+
+import com.example.pokedex.entities.Pokemon;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface  PokemonRepository extends MongoRepository<Pokemon, String> {
+}
