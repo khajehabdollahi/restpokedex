e29f1ba354e1000a1f05e61cbef275f6425b6c27
Hassan <pkhajeh@gmail.com>
2021-11-24 11:45:08 +0100

    add pokemon types, & abilities entities

---
+++ src/main/java/com/example/pokedex/entities/Ability.java
@@ -0,0 +1,16 @@
+package com.example.pokedex.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ability {
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("url")
+    private String url;
+}
--- src/main/java/com/example/pokedex/entities/Pokemon.java
+++ src/main/java/com/example/pokedex/entities/Pokemon.java
@@ -1,63 +1,23 @@
 package com.example.pokedex.entities;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
-@Data
-@Builder
-public class Pokemon {
-    private static final long serialVersionUID = 669636611022282531L;
+import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pokemon {
     @Id
     private String id;
+    @JsonProperty("name")
     private String name;
-    private int height;
-    private int base_stat;
-
-    public Pokemon() {
-    }
-
-    public Pokemon(String id, String name, int height, int base_stat) {
-        this.id = id;
-        this.name = name;
-        this.height = height;
-        this.base_stat = base_stat;
-    }
-
-    public static long getSerialVersionUID() {
-        return serialVersionUID;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public int getBase_stat() {
-        return base_stat;
-    }
-
-    public void setBase_stat(int base_stat) {
-        this.base_stat = base_stat;
-    }
+    private String height;
+    private String weight;
+    private List<Abilities> abilities;
+    private List<Types> types;
 }
